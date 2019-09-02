8bc636929e42b40bd46853fee06a0491f3497ae0
Outalianh Mylondo <outalianhsnakebouya@msn.com>
2019-09-02 17:43:12 +0200

    db created

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,13 @@ ActiveRecord::Schema.define(version: 2019_09_02_150343) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "carts", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
+
   create_table "items", force: :cascade do |t|
     t.string "title"
     t.text "description"
