eafaafd46a399e16dad7f523a8eb180930132795
Outalianh Mylondo <outalianhsnakebouya@msn.com>
2019-09-02 16:35:08 +0200

    removing element from table

--- db/migrate/20190902135033_create_orders.rb
+++ db/migrate/20190902135033_create_orders.rb
@@ -1,9 +1,7 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
-    	has_many :item_orders
-    	has_many :items, through: :item_orders
-
+    	
       t.timestamps
     end
   end
