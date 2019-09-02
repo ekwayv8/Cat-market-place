eb329bd962153a2823211fbbc86d03ca3e041ecd
Outalianh Mylondo <outalianhsnakebouya@msn.com>
2019-09-02 16:21:41 +0200

    Added db orders

--- db/migrate/20190902135033_create_orders.rb
+++ db/migrate/20190902135033_create_orders.rb
@@ -1,6 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
+    	has_many :item_orders
+    	has_many :items, through: :item_orders
 
       t.timestamps
     end
