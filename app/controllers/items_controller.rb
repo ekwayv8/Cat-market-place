88c0e5aa52923a5e6e10b1087a08387f3c669637
Outalianh Mylondo <outalianhsnakebouya@msn.com>
2019-09-03 16:47:42 +0200

    Edited Item controller: to be checked

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,19 +1,38 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all   
   end
 
   def show
+    @item = Item.find(params[:title])
   end
 
   def new
+    @item = Item.new 
   end
 
   def create
+    @item = Item.new(item_params)
+      if @item.save
+        redirect_to @item
+      else
+        render 'new'
+      end
   end
 
   def update
+  
   end
 
   def destroy
+    @item = Item.destroy
+
   end
+
+private
+
+  def item_params
+    params.require(:item).permit(:title, :description, :price, :image_url)
+  end
+
 end
