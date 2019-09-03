aa8e23231539110d4180143c4183c133b1c2f0d7
Outalianh Mylondo <outalianhsnakebouya@msn.com>
2019-09-03 17:34:39 +0200

    Changed title to id and the path for the redirect in item controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:title])
+    @item = Item.find(params[:id])
   end
 
   def new
@@ -14,7 +14,7 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
       if @item.save
-        redirect_to @item
+        redirect_to item_path
       else
         render 'new'
       end
