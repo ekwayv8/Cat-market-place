681bef1a7bdd08f179946810671eeb9f1eef4e77
ekwayv8 <yvon.ekwalla@gmail.com>
2019-09-04 10:18:43 +0200

    Modification of the basics routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,11 @@
 Rails.application.routes.draw do
-  get 'orders/new'
-  get 'orders/show'
-  get 'orders/create'
-  get 'orders/update'
-  get 'orders/destroy'
-  get 'carts/new'
-  get 'carts/show'
-  get 'carts/create'
-  get 'carts/update'
-  get 'carts/destroy'
-  get 'items/index'
-  get 'items/show'
-  get 'items/new'
-  get 'items/create'
-  get 'items/update'
-  get 'items/destroy'
+
   devise_for :users
+  root to: 'items#index'
+  resources :carts
+  resources :items
+  resources :orders
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
