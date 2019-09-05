7177b9f441d15f4f2601c6d342daaf9bba828425
ekwayv8 <yvon.ekwalla@gmail.com>
2019-09-05 10:42:39 +0200

    Passing the API KEY for mailjet to heroku

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['MAILJET_LOGIN'],
   :password => ENV['MAILJET_PWD'],
-  :domain => 'cat-marketplace-staging.herokuapp.com/',
+  :domain => 'cat-marketplace-prod.herokuapp.com/',
   :address => 'in-v3.mailjet.com',
   :port => 587,
   :authentication => :plain,
