3868e95e10ab233c0852e76500e0c96cedddf910
ekwayv8 <yvon.ekwalla@gmail.com>
2019-09-05 10:37:45 +0200

    Adding app key in the .env file and also some config in order for it to work

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#SMTP parameters for Mailjet
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['MAILJET_LOGIN'],
+  :password => ENV['MAILJET_PWD'],
+  :domain => 'cat-marketplace-staging.herokuapp.com/',
+  :address => 'in-v3.mailjet.com',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
