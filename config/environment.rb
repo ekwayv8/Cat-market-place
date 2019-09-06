499faeed0fafe87c650f9d88b50a910694d5eb19
ekwayv8 <yvon.ekwalla@gmail.com>
2019-09-06 03:18:27 +0200

    Modification of the setup for mailjet

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['MAILJET_LOGIN'],
   :password => ENV['MAILJET_PWD'],
-  :domain => 'cat-marketplace-prod.herokuapp.com/',
+  :domain => 'laposte.net',
   :address => 'in-v3.mailjet.com',
   :port => 587,
   :authentication => :plain,
